70494fcdef61b07a0c190f37051f733522ba90c6
Chipsoft1 <z.kadioglu@chipsoft.com>
2024-05-25 16:26:59 +0200

    Create test_example_script.ps1

---
+++ site-modules/my_tasks/tasks/test_example_script.ps1
@@ -0,0 +1,25 @@
+# Output message
+$output = Write-Output "Hello, this is a test script from Puppet!"
+$output | Out-File -FilePath C:\Scripts\output-puppet-test.txt
+
+Exit 0
+
+# Start of the script
+$logPath = "C:\Scripts\ExampleScript.log"
+
+# Function to write messages to the log file
+function Write-Log {
+    param ([string]$message)
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Add-Content -Path $logPath -Value "${timestamp}: $message"
+}
+
+# Log the start of the script
+Write-Log "Script Execution Started"
+
+# Sample command in the script
+Write-Log "Running Command 1"
+# [Place your existing script commands here]
+
+# Log the end of the script
+Write-Log "Script Execution Completed
