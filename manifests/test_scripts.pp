948a5ac9e3566930cd0fd443893fd3a30e117732
Chipsoft1 <z.kadioglu@chipsoft.com>
2024-05-25 16:39:25 +0200

    Create test_scripts.pp

---
+++ manifests/test_scripts.pp
@@ -0,0 +1,6 @@
+class test_scripts {
+  exec { 'test_example_script':
+    command   => 'powershell.exe -ExecutionPolicy Bypass -File "C:/Scripts/test_example_script.ps1"',
+    provider  => powershell,
+    logoutput => true,
+  }
