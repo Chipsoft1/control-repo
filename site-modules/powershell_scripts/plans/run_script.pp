0b4842319ddbf40d69187447484a3c0a6cb0aa3c
Chipsoft1 <z.kadioglu@chipsoft.com>
2024-05-25 11:47:29 +0200

    Update run_script.pp

--- site-modules/powershell_scripts/plans/run_script.pp
+++ site-modules/powershell_scripts/plans/run_script.pp
@@ -4,10 +4,10 @@ plan powershell_scripts::run_script(
   $results = run_task('powershell_scripts::script_one', $nodes)
   $results.each |$result| {
     if $result['status'] != 'success' {
-      if $result.key?('_error') {
-        fail_plan("script_one failed on ${result['target']}: ${result['_error']['msg']}. Full error: ${result['_error']}")
+      if $result['_error'] {
+        fail_plan("script_one failed on ${result['target']}: ${result['_error']['msg']}")
       } else {
-        fail_plan("script_one failed on ${result['target']} with unknown error. Full result: ${result}")
+        fail_plan("script_one failed on ${result['target']} with unknown error.")
       }
     }
   }
