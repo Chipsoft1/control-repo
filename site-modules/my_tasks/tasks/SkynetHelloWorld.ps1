20d7932089fdb9088245265c482b406f9d11f51e
Chipsoft1 <z.kadioglu@chipsoft.com>
2024-05-30 10:31:57 -0400

    Update SkynetHelloWorld.ps1

--- site-modules/my_tasks/tasks/SkynetHelloWorld.ps1
+++ site-modules/my_tasks/tasks/SkynetHelloWorld.ps1
@@ -1,1 +1,18 @@
-﻿Write-Host "Hello Skynet"
+$output = Write-Output "Hello, this is a test script from Puppet! Duane"
+$output | Out-File -FilePath C:\Scripts\output-puppet.txt
+
+Exit 0
+
+$logPath = "C:\Scripts\ExampleScript.log"
+
+function Write-Log {
+    param ([string]$message)
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Add-Content -Path $logPath -Value "${timestamp}: $message"
+}
+
+Write-Log "Script Execution Started"
+
+Write-Log "Running Command 1"
+
+Write-Log "Script Execution Completed"
